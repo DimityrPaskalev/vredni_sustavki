23551705e332c41930ba89508cf60545a646d5f3
DimityrPaskalev <ddpaskalev24a@gmail.com>
2026-04-21 14:12:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,22 @@ import easyocr
 from PIL import Image
 import re
 import numpy as np
-from deep_translator import GoogleTranslator
 
-st.title("OCR + Превод + Откриване на вредни съставки")
+st.title("OCR за разпознаване на Е-та и вредни съставки")
 
+# Вредни съставки (английски + български)
 bad_ingredients = [
-    "palm oil", "E621", "палмово масло", "E102", "E133", "E250",
-    "E211", "E951", "E407", "E110", "E129", "E200-E299",
-    "E471", "E472", "HFCS", "white flour", "modified starch",
-    "artificial flavors", "E450", "E338", "E320", "MSM", "processed cheese"
+    "palm oil", "палмово масло",
+    "E621", "E102", "E133", "E250", "E211", "E951",
+    "E407", "E110", "E129",
+    "E471", "E472",
+    "HFCS", "глюкозо-фруктозен сироп",
+    "white flour", "бяло брашно",
+    "modified starch", "модифицирано нишесте",
+    "artificial flavors", "изкуствени аромати",
+    "E450", "E338", "E320",
+    "MSM",
+    "processed cheese", "топено сирене", "консерванти"
 ]
 
 uploaded_file = st.file_uploader("Качи снимка", type=["png", "jpg", "jpeg"])
@@ -32,14 +39,6 @@ if uploaded_file is not None:
 
     st.subheader("📄 Разпознат текст:")
     st.write(extracted_text)
-
-    # Превод
-    try:
-        translated_text = GoogleTranslator(source='auto', target='en').translate(extracted_text)
-        st.subheader("🌍 Превод на английски:")
-        st.write(translated_text)
-    except:
-        st.warning("Грешка при превода.")
 
     # Търсене на Е-та
     e_numbers = re.findall(r'\bE\d{3,4}\b', extracted_text.upper())
